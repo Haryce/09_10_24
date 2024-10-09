89eb92626418a15724a8c3812b9da78feb72e425
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-09 15:35:02 +0900

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int orelCount = 0;
+    cout << "Введите результаты подбрасывания монетки (1 - орел, 0 - решка):\n";
+    for (int i = 0; i < 9; ++i) {
+        int result;
+        cin >> result;
+
+        if (result != 0 && result != 1) {
+            cout << "Некорректный ввод. Введите 1 или 0.\n";
+            return 1; // Прерываем программу при некорректном вводе
+        }
+        if (result == 1) {
+            orelCount++;
+        }
+    }
+    if (orelCount % 2 == 0) {
+        cout << "Студент Д. принял положительное решение.\n";
+    }
+    else {
+        cout << "Студент Д. принял отрицательное решение.\n";
+    }
+    return 0;
+}
