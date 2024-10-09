49ff6a8f17329c74be4a344605b1014aae742238
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-09 15:13:15 +0900

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number;
+    cout << "Введите число больше нуля: ";
+    cin >> number;
+    // Проверка на корректность ввода
+    if (number <= 0) {
+        cout << "Ошибка: число должно быть больше нуля." << endl;
+        return 1;
+    }
+    // Вывод цифр в обратном порядке
+    while (number > 0) {
+        cout << number % 10;
+        number /= 10; // Деление на 10 для перехода к следующей цифре
+    }
+    cout << endl;
+    return 0;
+}
