058c17ab3f9f5a6b7e2582ce3ddca6c69d80da70
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-09 15:19:00 +0900

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int number, digit, sum = 0;
+    cout << "Введите число: ";
+    cin >> number;
+    while (number != 0) {
+        digit = number % 10;
+        sum += digit;
+        number /= 10;
+    }
+    cout << "Сумма цифр числа: " << sum << endl;
+
+    return 0;
+}
