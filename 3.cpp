c58f704be7c2766eef84d65af74dbb73f4709f4a
Haryce <148765923+Haryce@users.noreply.github.com>
2024-10-09 15:29:23 +0900

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int days;
+    cout << "Введите количество дней: ";
+    cin >> days;
+    int dd = 0; //вся прорйденная дистанция
+    int qw = 15; //дистанция пройденая за день
+
+    for (int i = 1; i <= days; i++) {
+        dd += qw;
+        qw += 2;
+    }
+    cout << "Общее расстояние, пройденное улиткой: " << dd << " см" << endl;
+
+    return 0;
+}
